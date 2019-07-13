918cdd423b8afdcfc5c2ab8a55f7dac474055dda
Pedro <pedro.luis.almeida.victor@gmail.com>
2019-07-12 23:48:52 -0400

    languages first init and redirect url

---
+++ js/booksjs.js
@@ -0,0 +1,7 @@
+var books = {
+    "book_author_1": "António Damásio"
+    "book_title": "Erro de Descartes"
+    "book_cover": ""
+}
+var output = document.getElementById('output');
+output.innerHTML = books.book_title_1;
